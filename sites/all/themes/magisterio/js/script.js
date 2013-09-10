a9abd36b5d25ab3a5166b4acc16a60d4a3eea791
Sergio Vargas <sergio.svargass@gmail.com>
2013-09-10 13:40:20 -0500

    menu topicos

--- sites/all/themes/magisterio/js/script.js
+++ sites/all/themes/magisterio/js/script.js
@@ -1,16 +1,16 @@
 (function ($) {
-	/*$(document).ready(function(){
+	$(document).ready(function(){
 		$('.expanded','#block-system-main-menu').hover(function(){
 			$('ul',this).show()
 		},function(){
 			$('ul',this).hide()
 		})
 
-		$('.views-row, .views-row *','.view-t-picos').hover(function(){
-			$('.views-field-nothing', this).show()
-		}, function(){
-			var padre = this;
-			setTimeout(function(){$('.views-field-nothing', padre).hide()},500);
+		$('.views-row .left-menu-topcs ul li', '.view-t-picos').hover(function(){
+			var clase = $(this).attr('class');
+			clase = clase.replace('s-t','.r-t');
+			var padre = $(this).parents('.views-row');
+			console.log($(clase, padre).html());
 		})
-	})*/
+	})
 })(jQuery);
